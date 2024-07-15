4f92ff6311098398c385abd176c6c226080d8653
Karthik Reddy <karthikreddy311@gmail.com>
2024-07-14 21:28:41 -0700

    Updated homepage

--- src/components/views/Intro.vue
+++ src/components/views/Intro.vue
@@ -1,8 +1,8 @@
 <template>
         <v-card class="bkg-blur">
-            <p class="f2"> I'm Karthik Reddy (and also Batman in some cultures), a recent Ph.D graduate in astrophysics from UMBC, and a soon to be postdoc at Arizona State University.</p>
+            <p class="f2"> I'm Karthik, a postdoc at Arizona State University.</p>
                 <p class="f1_5">
-                    I work on extragalactic jets, fast radio bursts, astrostatistics and building scalable systems.
+                    I work on extragalactic jets, fast radio bursts, astrostatistics and high performance computing.
                 </p>
                 <p class="f1_5">
                     I also like to watch black hole lensing. <v-icon dark>mdi-arrow-down-right</v-icon>
