74e70eddf05b674dbf4587d9e70533557ffbb23e
Karthik Reddy <karthikreddy311@gmail.com>
2025-04-25 21:27:25 -0700

    update description

--- src/components/views/Intro.vue
+++ src/components/views/Intro.vue
@@ -1,12 +1,13 @@
 <template>
-        <v-card class="bkg-blur">
-            <p class="f2"> Hey there! I'm Karthik and I'm a postdoc at Arizona State University.</p>
-                <p class="f1_5">
-                    I try to understand the nature of extragalactic jets and fast radio bursts using astrostatistics and high performance computing.
-                </p>
-                <p class="f1_5">
-                    I also like to watch gravitational lensing. <v-icon dark>mdi-arrow-down-right</v-icon>
-                </p>
+    <v-card class="bkg-blur">
+        <p class="f2"> Hey there! I'm Karthik and I'm a GPU Engineer at Caltech.</p>
+        <p class="f1_5">
+            I do a bit of High-performance computing for DSA-2000. I also try
+            to understand the nature of extragalactic jets and fast radio bursts.
+        </p>
+        <p class="f1_5">
+            I also like to watch gravitational lensing. <v-icon dark>mdi-arrow-down-right</v-icon>
+        </p>
     </v-card>
 
 </template>
@@ -19,22 +20,21 @@ export default {
 
 <style>
 .bkg-blur {
-    background-color: rgba(0, 0, 0, 0.1)!important;
+    background-color: rgba(0, 0, 0, 0.1) !important;
     backdrop-filter: blur(2px);
     /* position: absolute; */
     color: white !important;
     padding: 2.5%;
-    top:1% !important;
-    text-align:center;
-    width:100%;
+    top: 1% !important;
+    text-align: center;
+    width: 100%;
 }
 
-.f2{
+.f2 {
     font-size: 2em;
 }
 
-.f1_5{
-    font-size:1.5em;
+.f1_5 {
+    font-size: 1.5em;
 }
-
 </style>
